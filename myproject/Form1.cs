6807665ca74c49e9cef1fb8d287ce5b485d204e2
MR. Morningstar <ee8991471@gmail.com>
2025-12-28 22:18:41 +0600

    StudyMate main Panel Add - Emon

--- myproject/Form1.cs
+++ myproject/Form1.cs
@@ -40,8 +40,8 @@ namespace myproject
 
         private void button1_Click(object sender, EventArgs e)
         {
-            studyMateMain m = new studyMateMain();
-            m.Show();
+            //studyMateMain m = new studyMateMain();
+            //m.Show(); ** DO NOT TOUCH UNCOMMENT THIS ** 
         }
     }
 }
