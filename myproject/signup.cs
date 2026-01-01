59d77efbba7210bf5da13f3f1b17c6780987941d
MR. Morningstar <ee8991471@gmail.com>
2026-01-01 17:43:59 +0600

    Database with Signup page edited

--- myproject/signup.cs
+++ myproject/signup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -26,6 +27,8 @@ namespace myproject
         {
             
         }
+
+        // exit korar jonno
         protected override void OnFormClosed(FormClosedEventArgs e)
         {
             Application.Exit();
@@ -71,6 +74,12 @@ namespace myproject
             this.Hide();
         }
 
+        
+
+
+
+        // DATABASE CONNECTION - 
+        string connectionString = "data source=DESKTOP-BF5OMUT\\SQLEXPRESS; database=KK; integrated security=SSPI";
         private void button1_Click(object sender, EventArgs e)
         {
             string email = textBox1.Text;
@@ -80,7 +89,45 @@ namespace myproject
             if(password.Equals(match_passwordForCheck))
             {
 
+                if (string.IsNullOrWhiteSpace(email) 
+                    || string.IsNullOrWhiteSpace(user_name)
+                    || string.IsNullOrWhiteSpace(password) 
+                    || string.IsNullOrWhiteSpace(match_passwordForCheck))
+                {
+                    MessageBox.Show("All fields must be filled out.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
 
+                string query = "INSERT INTO Users (Email, UserName, Password) " +"VALUES (@Email, @UserName, @Password); " +
+               "SELECT SCOPE_IDENTITY();";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open(); 
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Email", email);
+                        command.Parameters.AddWithValue("@UserName", user_name);
+                        command.Parameters.AddWithValue("@Password", password);
+
+                     
+                        var result = command.ExecuteScalar();
+
+                        if (result != null)
+                        {
+                            int newUserId = Convert.ToInt32(result);
+                            MessageBox.Show("Sign UP successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            Form1 f = new Form1();
+                            f.ShowDialog();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Sign UP Failed.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
 
 
 
